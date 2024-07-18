c49df8cd35420f626c60c7f802c837d907f73a38
HPhp-02 <palharshit1967@gmail.com>
2024-07-18 17:32:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+    console.log("Harshit");
+     d = new Date();
+     hours=d.getHours();
+     minutes=d.getMinutes();
+     seconds=d.getSeconds();
+     hour_hand=30*hours+minutes/2;
+     minute_hand=6*minutes+seconds/10;
+     second_hand=6*seconds;
+    hour.style.transform=`rotate(${hour_hand}deg)`; 
+    minute.style.transform=`rotate(${minute_hand}deg)`; 
+    second.style.transform=`rotate(${second_hand}deg)`; 
+}, 1000);
